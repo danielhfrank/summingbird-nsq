fe684f533333e79fd85186d2863a5f78514633b4
Dan Frank <df@stripe.com>
2015-07-15 01:06:00 -0700

    Start work on example runner (still no clue how to pass through nsq info)

---
+++ src/main/scala/Example.scala
@@ -0,0 +1,38 @@
+import com.twitter.algebird.Semigroup
+import com.twitter.storehaus.ReadableStore
+import com.twitter.storehaus.algebra.Mergeable
+import com.twitter.summingbird.Source
+import com.twitter.util.Future
+
+import scala.util.Try
+
+object Example {
+
+  private class FakeReadableStore extends ReadableStore[String, Int] with Mergeable[String, Int]{
+
+    private val intMap = new scala.collection.mutable.HashMap[String, Int]
+
+    override def get(k: String): Future[Option[Int]] = Future.value(intMap.get(k))
+
+    override def semigroup: Semigroup[Int] = Semigroup.intSemigroup
+
+    override def merge(kv: (String, Int)): Future[Option[Int]] = {
+      val (k,v) = kv
+      val prevV = intMap.get(k)
+      val newV = semigroup.sumOption(Seq(Some(v), prevV).flatten).get
+      intMap.put(k, newV)
+      Future.value(prevV)
+    }
+  }
+
+  def main (args: Array[String]) {
+    val source = new NSQSource[String]("test", "summingbird-nsq", bytes => Try(new String(bytes, "UTF-8")).toOption)
+    val sbSource = Source[NSQ, String](source)
+    val store = new FakeReadableStore
+    val mapped = sbSource.map{ s => (s, 1)}
+    val summed = mapped.sumByKey(store)
+
+    val stream = (new NSQ).plan(summed)
+  }
+
+}
