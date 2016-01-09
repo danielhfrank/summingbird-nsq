f87e76b22113b2244ea9ff89bc5231818fac3b1e
Dan Frank <df@stripe.com>
2016-01-08 16:22:05 -0800

    Add NSQDriver to work with push-based platform (ie Receiver)

---
+++ src/main/scala/NSQDriver.scala
@@ -0,0 +1,26 @@
+import com.trendrr.nsq.{NSQMessage, NSQMessageCallback}
+import com.twitter.summingbird.TimeExtractor
+import com.twitter.summingbird.batch.Timestamp
+import com.twitter.util.Try
+
+/**
+  * This class handles receiving messages from nsqd, pushing them into a topology,
+  * and ack/req-ing them when the topology is finished
+  */
+class NSQDriver[T: TimeExtractor](receiver: Receiver[T])(decodeFn: (Array[Byte]) => Try[T]) extends NSQMessageCallback {
+
+  val te = implicitly[TimeExtractor[T]]
+
+  override def error(x: Exception): Unit = {}
+
+  override def message(message: NSQMessage): Unit = {
+    val tryMessageValue: Try[T] = decodeFn(message.getMessage)
+    tryMessageValue.map { messageValue =>
+      val timestamp = Timestamp(te(messageValue))
+      val pushResult = receiver.push((timestamp, messageValue))
+      pushResult
+        .onSuccess(_ => message.finished())
+        .onFailure(_ => message.requeue())
+    }.onFailure { t => println("Could not decode " + new String(message.getMessage, "UTF-8")) }
+  }
+}
